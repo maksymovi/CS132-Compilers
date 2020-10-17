4a75c8c2a36371fe38e5f2f8f2d828cd99411f91
Maksym Prokopovych <24415429+maksymovi@users.noreply.github.com>
2020-10-16 22:08:05 -0700

    Loaded parse table, committing to save

--- 1HW/src/main/java/Parse.java
+++ 1HW/src/main/java/Parse.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 
 public class Main {
 	public static void main(String [] args) {
-		
+		Lexer.main(null);
 	}
 
 }
@@ -26,35 +26,41 @@ public abstract class Token {
 		value = counter;
 		counter++;
 	}
-*/
-	
+	*/
+	private static int count = 0;
+	private int val;
+	private Token() {
+		val = count;
+		count++;
+	}
+
 	public abstract Type type();
 	
-	public static class Terminal extends Token {
+	public static class T extends Token {
 		public Type type() {
 			return Type.Terminal;
 		}//a little boilerplatey
-		public final static Terminal leftBrace = new Terminal();
-		public final static Terminal rightBrace = new Terminal();
-		public final static Terminal SOP = new Terminal();
-		public final static Terminal openParen = new Terminal();
-		public final static Terminal closeParen = new Terminal();
-		public final static Terminal semicolon = new Terminal();
-		public final static Terminal ifStatement = new Terminal();
-		public final static Terminal elseStatement = new Terminal();
-		public final static Terminal whileStatement = new Terminal();
-		public final static Terminal trueStatement = new Terminal();
-		public final static Terminal falseStatement = new Terminal();
-		public final static Terminal exclamation = new Terminal();
+		public final static T leftBrace = new T();
+		public final static T rightBrace = new T();
+		public final static T SOP = new T();
+		public final static T openParen = new T();
+		public final static T closeParen = new T();
+		public final static T semicolon = new T();
+		public final static T ifStatement = new T();
+		public final static T elseStatement = new T();
+		public final static T whileStatement = new T();
+		public final static T trueStatement = new T();
+		public final static T falseStatement = new T();
+		public final static T exclamation = new T();
 
 	}
-	public static class Nonterminal extends Token {
+	public static class N extends Token {
 		public Type type() {
 			return Type.Nonterminal;
 		}
-		public final static Nonterminal S = new Nonterminal();
-		public final static Nonterminal L = new Nonterminal();
-		public final static Nonterminal E = new Nonterminal();
+		public final static N S = new N();
+		public final static N L = new N();
+		public final static N E = new N();
 	}
 	public static class Nullable extends Token {
 		public Type type() {
@@ -66,50 +72,94 @@ public abstract class Token {
 }
 
 public class Parser {
-	/* private class TClass { //used for individual rules, there may be a better way to sort between terminals and nonterminals but eh
-		private TType type;
-		private T token;
-		public T getToken() {
-			return token;
+
+	public class TPair {
+		private Token.N N;
+		private Token.T T;
+		public TPair(Token.N NArg, Token.T TArg) {
+			N = NArg;
+			T = TArg;
 		}
-		public T getType() {
-			return type;
-		}
-		TClass(TType argType, T argToken) {
-			token = argToken;
-			type = argType;
-		}
-		
 	}
-	private class TPair { //used in parse table locations, dont even need getters for comparison purposes
-		private T n;
-		private T t;
-		TPair(T argN, T argT) {
-			n = argN;
-			t = argT;
-		}
-		}*/
-	
+
+
+	HashMap<TPair, List<Token>> grammar;
+
+	public void InitializeGrammar() {
+		grammar = new HashMap<TPair, List<Token>>();
+		
+
+		//S GRAMMARS
+		grammar.put(new TPair(Token.N.S, Token.T.leftBrace), Arrays.asList(
+						Token.T.leftBrace,
+						Token.N.L,
+						Token.T.rightBrace));
+		grammar.put(new TPair(Token.N.S, Token.T.SOP), Arrays.asList(
+						Token.T.SOP,
+						Token.T.openParen,
+						Token.N.E,
+						Token.T.closeParen,
+						Token.T.semicolon));
+		grammar.put(new TPair(Token.N.S, Token.T.ifStatement), Arrays.asList(
+						Token.T.ifStatement,
+						Token.T.openParen,
+						Token.N.E,
+						Token.T.closeParen,
+						Token.N.S,
+						Token.T.elseStatement,
+						Token.N.S));
+		grammar.put(new TPair(Token.N.S, Token.T.whileStatement), Arrays.asList(
+						Token.T.whileStatement,
+						Token.T.openParen,
+						Token.N.E,
+						Token.T.closeParen,
+						Token.N.S));
+		//L GRAMMARS
+		grammar.put(new TPair(Token.N.L, Token.T.leftBrace), Arrays.asList(
+						Token.N.S,
+						Token.N.L));
+		grammar.put(new TPair(Token.N.L, Token.T.rightBrace), Arrays.asList(
+						Token.Nullable.nullable));
+		grammar.put(new TPair(Token.N.L, Token.T.SOP), Arrays.asList(
+						Token.N.S,
+						Token.N.L));
+		grammar.put(new TPair(Token.N.L, Token.T.ifStatement), Arrays.asList(
+						Token.N.S,
+						Token.N.L));
+		grammar.put(new TPair(Token.N.L, Token.T.whileStatement), Arrays.asList(
+						Token.N.S,
+						Token.N.L));
+		//E GRAMMARS
+		grammar.put(new TPair(Token.N.E, Token.T.trueStatement), Arrays.asList(
+						Token.T.trueStatement));
+		grammar.put(new TPair(Token.N.E, Token.T.falseStatement), Arrays.asList(
+						Token.T.falseStatement));
+		grammar.put(new TPair(Token.N.E, Token.T.exclamation), Arrays.asList(
+						Token.T.exclamation,
+						Token.N.E));
+	}
 }
 
 
 public class Lexer {
-	/*public static void main(String [ ] args) {
-		ArrayList<T> terms = lex("");
+	public static void main(String [ ] args) {
+		ArrayList<Token.T> terms = lex("");
 		if(terms.isEmpty())
 		{
 			System.out.println("Error");
 		}
 		else
+			System.out.println("Valid");
+			/*
 			for(T t : terms)
 			{
 				System.out.println(t.name());
-			}
+				}*/
 		return;
-		}*/
-	public static ArrayList<Token.Terminal> lex (String program) { //returns empty arraylist if error
+		}
+	public static ArrayList<Token.T> lex (String program) { //returns empty arraylist if error
 
-		ArrayList<Token.Terminal> tList = new ArrayList<Token.Terminal>();
+		ArrayList<Token.T> tList = new ArrayList<Token.T>();
 		//Scanner s = new Scanner(program); //scanner removes whitespace, making lexing a lot easier
 		Scanner s = new Scanner(System.in); //POSSIBLE TESTING LINE THAT TAKES IN STDIN DIRECTLY
 		while(s.hasNext()) {
@@ -121,22 +171,22 @@ public class Lexer {
 				if(!Character.isLetter(line.charAt(0))) {
 						switch(line.charAt(0)) {//easy cases first
 						case '{':
-							tList.add(Token.Terminal.leftBrace);
+							tList.add(Token.T.leftBrace);
 							break;
 						case '}':
-							tList.add(Token.Terminal.rightBrace);
+							tList.add(Token.T.rightBrace);
 							break;
 						case '(':
-							tList.add(Token.Terminal.openParen);
+							tList.add(Token.T.openParen);
 							break;
 						case ')':
-							tList.add(Token.Terminal.closeParen);
+							tList.add(Token.T.closeParen);
 							break;
 						case ';':
-							tList.add(Token.Terminal.semicolon);
+							tList.add(Token.T.semicolon);
 							break;
 						case '!':
-							tList.add(Token.Terminal.exclamation);
+							tList.add(Token.T.exclamation);
 							break;
 						default:
 							//starts without letter but not a character here, error, grammar is invalid
@@ -153,27 +203,27 @@ public class Lexer {
 						}
 						hadletters = true;
 						if(line.startsWith("if")) { //there is probably a better way to do this than chaining if statements, might fix this later, though the code is simple this way
-							tList.add(Token.Terminal.ifStatement);
+							tList.add(Token.T.ifStatement);
 							line = line.substring("if".length());
 						}
 						else if(line.startsWith("else")) {
-							tList.add(Token.Terminal.elseStatement);
+							tList.add(Token.T.elseStatement);
 							line = line.substring("else".length());
 						}
 						else if(line.startsWith("while")) {
-							tList.add(Token.Terminal.whileStatement);
+							tList.add(Token.T.whileStatement);
 							line = line.substring("while".length());
 						}
 						else if(line.startsWith("true")) {
-							tList.add(Token.Terminal.trueStatement);
+							tList.add(Token.T.trueStatement);
 							line = line.substring("true".length());
 						}
 						else if(line.startsWith("false")) {
-							tList.add(Token.Terminal.falseStatement);
+							tList.add(Token.T.falseStatement);
 							line = line.substring("false".length());
 						}
 						else if(line.startsWith("System.out.println")) {
-							tList.add(Token.Terminal.SOP);
+							tList.add(Token.T.SOP);
 							line = line.substring("System.out.println".length());
 						}
 						else {
